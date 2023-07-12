c8146f7d6702de1196c661775bc129e4f78b93c6
deepti sharma <124440255+deeptisharma10@users.noreply.github.com>
2023-07-12 23:30:18 +0530

    new code added

--- js/custom.js
+++ js/custom.js
@@ -1,56 +1,27 @@
-/*global jQuery:false */
+// /*global jQuery:false */
+// var sitebasepath = "js/";
+
+// includejs = function (url) {
+//   document.write('<script src="' + url + '"></script>');
+// };
+
+// includejs(sitebasepath+"jquery.min.js");
+// includejs(sitebasepath + "bootstrap.bundle.min.js");
+// includejs(sitebasepath + "slick.js");
+
+// addAllListeners = function () {};
+
 var sitebasepath = "js/";
 
 includejs = function (url) {
   document.write('<script src="' + url + '"></script>');
 };
 
-includejs(sitebasepath+"jquery.min.js");
-includejs(sitebasepath + "bootstrap.bundle.min.js");
+// includejs(sitebasepath+"jquery.min.js");
+// includejs(sitebasepath + "bootstrap.bundle.min.js");
 includejs(sitebasepath + "slick.js");
 
 addAllListeners = function () {};
-
-$(".services-slider").slick({
-    dots: false,
-    infinite: true,
-    autoplay: false,
-    arrows: true,
-    autoplaySpeed: 7000,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    accessibility: false,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  });
 
   $("#servicesSlider").slick({
     dots: false,
@@ -94,54 +65,14 @@ $(".services-slider").slick({
   });
 
 
-wow = new WOW({
-	boxClass: "wow", // default
-	animateClass: "animated", // default
-	offset: 0, // default
-	mobile: true, // default
-  });
-  wow.init()
+// wow = new WOW({
+// 	boxClass: "wow", // default
+// 	animateClass: "animated", // default
+// 	offset: 0, // default
+// 	mobile: true, // default
+//   });
+//   wow.init()
 
-  $(".slider-img").slick({
-    dots: false,
-    infinite: true,
-    autoplay: false,
-    arrows: true,
-    autoplaySpeed: 7000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    accessibility: false,
-    // responsive: [
-    //   {
-    //     breakpoint: 1024,
-    //     settings: {
-    //       slidesToShow: 3,
-    //       slidesToScroll: 1,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 992,
-    //     settings: {
-    //       slidesToShow: 2,
-    //       slidesToScroll: 1,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 767,
-    //     settings: {
-    //       slidesToShow: 2,
-    //       slidesToScroll: 1,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 480,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1,
-    //     },
-    //   },
-    // ],
-  });
 
 
   $("#sliderImg").slick({
@@ -153,35 +84,18 @@ wow = new WOW({
     slidesToShow: 1,
     slidesToScroll: 1,
     accessibility: false,
-    // responsive: [
-    //   {
-    //     breakpoint: 1024,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 992,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 767,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 480,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1,
-    //     },
-    //   },
-    // ],
   });
 
+  $(document).ready(function () {
+    $("#slickHero").slick({
+      dots: true,
+      autoplay: true,
+      infinite: true,
+      autoplaySpeed: 5000,
+      arrows: false,
+      speed: 1000,
+      fade: true,
+      adaptiveHeight: true,
+      cssEase: "linear",
+    });
+  })
